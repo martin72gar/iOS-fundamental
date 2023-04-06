406b5ae871a70f9511d163a358e87674b727c7f7
Martin Siregar <72garmartin@gmail.com>
2023-04-06 09:46:32 +0700

    Initial Commit

---
+++ MyDicoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDicoding
+//
+//  Created by Osmartin Pardomuan Siregar on 06/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
